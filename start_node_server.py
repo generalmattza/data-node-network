f5fb0dfc71afd47ced9b6cba40ab2af6cb215bed
Matthew Davidson <matthew@davidson.engineering>
2024-02-18 10:33:46 -0800

    updated node-config

--- start_node_server.py
+++ start_node_server.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 
 async def main():
-    address = ("pizero2.local", 50_000)
+    address = ("10.0.0.141", 50_000)
     node_id = 0
 
     # Create and start the server
